089f0eab60287e211e2b92ea37ec2f92cd32f359
github username <github email address>
2021-01-21 10:07:27 -0800

    after lecture 11

--- 11-intro-orms/lib/tweet.rb
+++ 11-intro-orms/lib/tweet.rb
@@ -1,6 +1,6 @@
 class Tweet
   attr_accessor :message, :username, :id
-  # @@all = []
+  # @@all = []  # replaced by db
 
   def self.all
     # @@all
@@ -18,19 +18,20 @@ class Tweet
       SELECT * FROM tweets
       WHERE id = ?
     SQL
-    self.new(DB[:conn].execute(sql, id).first)
+    self.new(DB[:conn].execute(sql, id).first) # the db still returns the found hash in an array as [{...}], so calling .first returns the first element of the array as a bare hash
   end
 
   def initialize(attrs={})
     @message = attrs["message"]
     @username = attrs["username"]
     @id = attrs["id"]
-    # @@all << self
+    # @@all << self # replaced by db
   end
 
   def save
-    # if the instance already exists in db
-    if self.id
+    # if the instance already exists in db, update it
+    # a tweet instance will only have an id if it has previously been saved to the db
+    if self.id # nil values here will evaluate as falsy, anything else as truthy
       sql = <<-SQL
           UPDATE tweets
            SET message = ?
@@ -39,11 +40,14 @@ class Tweet
       DB[:conn].execute(sql, self.message, self.id)
       # updated
     else
-      # create a new one
+      # create a new one, if the tweet did NOT have an id
       sql = <<-SQL
           INSERT INTO tweets (username, message)
           VALUES (?, ?);
       SQL
+      # using ?, ? to sanitize the user data which we're sending to the db
+      # prevents SQL injection attacks
+      # the actual values are submitted as arguments to .execute below
       DB[:conn].execute(sql, self.username, self.message)
     end
   end
