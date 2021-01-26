5b701123d384234ca968f450097316027a775b97
github username <github email address>
2021-01-26 14:18:15 -0800

    before lecture 15

---
+++ 15-regex/regex.rb
@@ -0,0 +1,28 @@
+
+original_string = "wuefghklsvj745-678-4346bdfhgiujkbserigjsknf&df232.392.1424"
+
+new_string = original_string.match(/[78]/)
+# new_string = original_string.scan(/[78]/)
+# new_string = original_string =~ /[78]/
+
+# If a match is found, the index of first match in string is returned.
+
+
+puts new_string
+# puts original_string
+
+
+# iterators???? gsub!
+
+lotr = "the lord of the rings"
+
+surprise = lotr.gsub("the", "surprise")
+# surprise = lotr.gsub(/[a-z]/, "surprise")
+# g => global, sub => substitution
+
+capitalized = lotr.gsub(/\w+/) { |w| w.capitalize }
+# + => repeated
+
+# puts surprise
+
+# puts capitalized
