b9260d4bad1384ecadb69f00bd98f750c0f4dfbb
github username <github email address>
2021-02-05 11:41:43 -0800

    before lecture 21

---
+++ associations-forms/app/controllers/patients_controller.rb
@@ -0,0 +1,43 @@
+class PatientsController < ApplicationController
+
+    before_action :set_patient, only: [:show, :edit, :update, :destroy]
+
+    def index
+        @patients = Patient.all
+    end
+
+    def show
+    end
+
+    def new
+        @patient = Patient.new
+    end
+
+    def create
+        @patient = Patient.create(patient_params)
+        redirect_to patients_path
+    end
+
+    def edit
+    end
+
+    def update
+        @patient.update(patient_params)
+        redirect_to patient_path(@patient)
+    end
+
+    def destroy
+        @patient.destroy
+        redirect_to patients_path
+    end
+    
+    private
+
+    def patient_params
+        params.require(:patient).permit(:name, :condition)
+    end
+
+    def set_patient
+        @patient = Patient.find(params[:id])
+    end
+end
