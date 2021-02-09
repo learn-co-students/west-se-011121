51026f0970cb9147c46dbe7c06a373e460f29430
github username <github email address>
2021-02-08 17:19:20 -0800

    adds index view/action for employees

--- dunder-mifflin-rails-review/app/controllers/employees_controller.rb
+++ dunder-mifflin-rails-review/app/controllers/employees_controller.rb
@@ -1,2 +1,40 @@
 class EmployeesController < ApplicationController
+
+    before_action :set_employee, only: [:show]
+
+    def index
+        @employees = Employee.all
+    end
+
+    def show
+
+    end
+
+    def new
+
+    end
+    
+    def create
+
+    end
+
+    def edit
+
+    end
+
+    def update
+
+    end
+
+    private
+
+    def employee_params
+
+    end
+
+    def set_employee
+        @employee = Employee.find(params[:id])
+    end
+
+
 end
