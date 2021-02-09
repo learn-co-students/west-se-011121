65d124823f93dffaca87917c5e1dcc7cd0ef9940
github username <github email address>
2021-02-09 15:49:39 -0800

    deletes allergies and add validation

--- 24-recipe-allergen-review/app/models/allergy.rb
+++ 24-recipe-allergen-review/app/models/allergy.rb
@@ -1,4 +1,6 @@
 class Allergy < ApplicationRecord
   belongs_to :user
   belongs_to :ingredient
+
+  validates :ingredient, uniqueness: {scope: :user}
 end
--- 24-recipe-allergen-review/app/models/user.rb
+++ 24-recipe-allergen-review/app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
     has_many :recipes, dependent: :destroy
     has_many :allergies, dependent: :destroy
     has_many :ingredients, through: :allergies
+
+    validates :name, length: {minimum: 2}
 end
