69e876097847742a57552811dfb2d802c107d260
github username <github email address>
2021-02-09 14:35:38 -0800

    adds show for recipes

--- 24-recipe-allergen-review/app/controllers/recipes_controller.rb
+++ 24-recipe-allergen-review/app/controllers/recipes_controller.rb
@@ -1,31 +1,38 @@
 class RecipesController < ApplicationController
 
+    before_action :set_recipe, only: [:show, :edit, :update, :destroy]
+
     def index
         @recipes = Recipe.all
     end
 
     def show
-
     end
-
+    
     def new
-
+        
     end
-
+    
     def create
-
+        
     end
-
+    
     def edit
-
+        
     end
-
+    
     def update
-
+        
     end
-
+    
     def destroy
-
+        
+    end
+    
+    private
+    
+    def set_recipe
+        @recipe = Recipe.find(params[:id])
     end
 
 end
