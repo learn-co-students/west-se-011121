ab95d38bb614f4c464190a6dcf6978f043fcee01
github username <github email address>
2021-02-11 17:28:47 -0800

    after lecture 26

--- 26-sessions-cookies/app/controllers/responses_controller.rb
+++ 26-sessions-cookies/app/controllers/responses_controller.rb
@@ -1,11 +1,29 @@
 class ResponsesController < ApplicationController
 
-    before_action :define_question, :define_response
+    before_action :define_question, :define_response, :define_number_correct, :define_message
 
     def new
 
     end
 
+    def create
+        # session[:number_correct] = session[:number_correct] || 0
+        session[:number_correct] ||=  0
+        response = Response.create(response_params)
+        if response.question.correct_answer == response.answer
+            session[:number_correct] = session[:number_correct] + 1
+            puts "Correct"
+            flash[:message] = "Correct"
+        else
+            puts "Incorrect"
+            flash[:message] = "Incorrect"
+        end
+        puts "Total Correct: #{session[:number_correct]}"
+        redirect_to '/random-question'
+    end
+    
+    private
+    
     def define_question
         @question = Question.order("RANDOM()").limit(1)[0]
     end
@@ -14,16 +32,12 @@ class ResponsesController < ApplicationController
         @questionResponse = Response.new({ question: @question })
     end
 
-    def create
-        @number_correct = 0
-        response = Response.create(response_params)
-        if response.question.correct_answer == response.answer
-            @number_correct = @number_correct + 1
-            puts "Correct"
-        else
-            puts "Incorrect"
-        end
-        redirect_to '/random-question'
+    def define_number_correct
+        @number_correct = session[:number_correct]
+    end
+
+    def define_message
+        @message = flash[:message]
     end
 
     def response_params
