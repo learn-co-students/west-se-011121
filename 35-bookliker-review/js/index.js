d205f15a3048f09f39872bf9f4ccbb0b2ac18941
github username <github email address>
2021-03-01 14:28:31 -0800

    add initial fetch

--- 35-bookliker-review/js/index.js
+++ 35-bookliker-review/js/index.js
@@ -1,18 +1,35 @@
+// declare constants i.e. url
+const baseURL = "http://localhost:3000/books"
+
+
 document.addEventListener("DOMContentLoaded", function() {
     // Declare variables
         // set references to DOM nodes where we can append our own elements
+    const list = document.querySelector("#list")
+    const show = document.querySelector("#show-panel")
 
     // Register Event Listeners
         // add event listers to above as needed (click on title, click on LIKE)
 
     // Standalone Functions
        // fetch all books
+       function getAllBooks(){
+           fetch(baseURL)
+            .then(res => res.json())
+            .then(books => listBooks(books))
+       }
 
        // fetch one book
 
        // fetch a user
 
        // patch fetch to update likes
+
+       // create <li></li> for each book and add to DOM
+       function listBooks(books){
+           console.log('books: ', books);
+
+       }
 
        // handleTitleClick
 
@@ -21,5 +38,5 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // Function call(s)
         // initial call to fetch all books
-
+       getAllBooks()
 });
