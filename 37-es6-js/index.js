22e3ee741b61856b19a82c5f6aa69bc5840f18d9
github username <github email address>
2021-03-02 11:33:12 -0800

    before lecture 37

---
+++ 37-es6-js/index.js
@@ -0,0 +1,15 @@
+// destructuring
+
+// key value assignment shortcut
+
+// Spread Operator
+
+// all the forms of arrow functions
+
+// passing functions as arguments (callbacks) and ES6 interators(map, reduce, forEach, filter, find, etc)
+
+// function binding vs. arrow functions
+
+// class instance properties and class syntax in general
+
+// dynamic object keys
